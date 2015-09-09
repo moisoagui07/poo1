7bfa9ffb8e02b437c1a6bad7e02fe8d897fa5ad8
Moi Soagui <moisoagui@MacBook-Pro-de-Moi.local>
2015-09-09 10:36:02 -0500

    Ejercicio 1 parte 2 hospital

---
+++ ejercicio1hospital.scala
@@ -0,0 +1,13 @@
+println("Hospital publico")
+println("Ingrese el monto a repartir")
+val monto=readFloat()
+println("De la siguiente manera se distrubuira el dinero")
+println("Ginecologia 40%")
+val gine=(monto/100)*40
+println(" " + gine)
+println("Traumatologia 30%")
+val traumat=(monto/100)*30
+println(" " + traumat)
+println("Pediatria 30%")
+val pediatria=(monto/100)*30
+println(" " + pediatria)
