06558f936bdbdb0caac1bf5d59059cdc2dca0c98
Moi Soagui <moisoagui@MacBook-Pro-de-Moi.local>
2015-09-09 10:56:40 -0500

    Ejercicio 1 parte 3 porcentaje

---
+++ ejercicio1porcentaje.scala
@@ -0,0 +1,16 @@
+println("Cantidad 1")
+val cantidad1=readFloat()
+println("Cantidad 2")
+val cantidad2=readFloat()
+println("Cantidad 3")
+val cantidad3=readFloat()
+val ctotal=cantidad1+cantidad2+cantidad3
+println("Porcentaje 1")
+val p1=cantidad1*100/ctotal
+println("  " + p1)
+println("Porcentaje 2")
+val p2=cantidad2*100/ctotal
+println(" " + p2)
+println("Porcentaje 3")
+val p3=cantidad3*100/ctotal
+println("  " + p3)
